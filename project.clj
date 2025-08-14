25c220af3db7294210e03c5b61f0fd6ade85e647
Hans-J. Schmid <knock@myopendoor.de>
2025-08-14 17:14:17 +0200

    cljfmt

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.clojure/data.avl "0.2.0"]
-                 [org.clojure/tools.reader "1.5.2"]
-                 [org.flatland/ordered "1.15.12"]
-                 [org.clojure/data.int-map "1.3.0"]
+  :dependencies [[org.clojure/clojure           "1.12.0"]
+                 [org.clojure/data.avl          "0.2.0"]
+                 [org.clojure/tools.reader      "1.5.2"]
+                 [org.flatland/ordered          "1.15.12"]
+                 [org.clojure/data.int-map      "1.3.0"]
                  [org.clojure/data.priority-map "1.2.0"]]
+  :plugins [[dev.weavejester/lein-cljfmt        "0.13.1"]]
   :main ^:skip-aot cheatsheet.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
